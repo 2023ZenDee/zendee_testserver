d034a5b39c0f4f289e3a87819193a6bdb9bf816b
재명 <ljm9894@dgsw.hs.kr>
2023-06-20 18:00:40 +0900

    :sparkles: 아이디 중복 체크

--- routes/users/index.js
+++ routes/users/index.js
@@ -3,14 +3,15 @@ const login = require('./login');
 const register = require('./signUp');
 const refreshToken = require('./refreshToken');
 const logout = require('./logout');
+const check = require('./idcheck');
 
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/check', check);
 //router.get('/token',token);
 router.get('/refreshToken',refreshToken);
 router.delete('/logout', logout);
-
 module.exports = router;
