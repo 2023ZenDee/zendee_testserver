cc47ce663f139c4568e9319d8f91a501d4f1b809
재명 <ljm9894@dgsw.hs.kr>
2023-06-20 23:01:01 +0900

    :sparkles: 토큰검증기능 추가

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 require('dotenv').config();
 const secret = process.env.SECRET_KEY;
-const authenticateUser = async (req, res, next) => {
+exports.authenticateUser = async (req, res, next) => {
     const accessToken = req.cookies.accessToken; // 쿠키에서 Access Token을 가져옴
   
     if (!accessToken) {
@@ -23,12 +23,16 @@ const authenticateUser = async (req, res, next) => {
         return res.status(401).json({ message: '유효하지 않은 사용자입니다.' });
       }
   
+
       // 요청 객체에 사용자 정보를 첨부하여 다음 미들웨어 또는 라우트 핸들러로 이동
       req.user = user;
-  
+      //console.log(req.user);
       next();
     } catch (err) {
+      if(err.name = "TokenExpiredError"){
+        return res.status(401).json({ message : 'AccessToken이 만료되었습니다. 다시 로그인 해주십시오.'})
+      }
       return res.status(401).json({ message: '유효하지 않은 Access Token입니다.' });
     }
   };
-exports.module = authenticateUser;  
+//exports.module = authenticateUser;  
