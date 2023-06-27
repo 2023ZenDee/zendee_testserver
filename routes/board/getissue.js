fe405cd74dcaff232a1413920017b7f7efea4380
재명 <ljm9894@dgsw.hs.kr>
2023-06-27 23:03:48 +0900

    :sparkles: 2km이내 게시물 조회

--- routes/board/getissue.js
+++ routes/board/getissue.js
@@ -1,7 +1,34 @@
-/*const getIssue = (req, res) => {
+const { PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const getIssue = async(req, res) => {
     try{
         const { latitude, longitude} = req.query;
 
+        const filteringIssue = await prisma.post.findMany({
+            where: {
+                latitude : {
+                    gte : parseFloat(latitude)- 0.02,
+                    lte : parseFloat(latitude) + 0.02,
+                },
+                longitude: {
+                    gte : parseFloat(longitude) - 0.02,
+                    lte : parseFloat(longitude) + 0.02,
+                },
+            }
+        });
         //현지 위치에서 2km 반경 이내에 게시물 필터링
+        res.status(200).send({
+            message : "게시물 조회 성공",
+            data : {
+                filteringIssue
+            }
+        })
+    }catch(err){
+        console.error(err);
+        res.status(500).send({ message : '게시물 조회 실패'});
     }
-}*/
+    
+}
+
+module.exports = getIssue
--- routes/board/index.js
+++ routes/board/index.js
@@ -2,8 +2,10 @@ const express =require('express');
 const issues = require('./issue');
 const router = express.Router();
 const { authenticateUser}  = require('../../middleware/authenticate');
+const getIssue = require('./getissue');
 
 
 router.post('/', authenticateUser ,issues);
+router.get('/mymap',authenticateUser, getIssue);
 
 module.exports = router;
