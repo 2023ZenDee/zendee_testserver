f9cbfcc651400a97c751698795ec32f37acd7d29
재명 <ljm9894@dgsw.hs.kr>
2023-07-03 23:49:49 +0900

    swagger 모듈 추가

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const userRouter = require('./users');
 const postRouter = require('./board');
-
+const likeRouter = require('./like');
 
 const router = express.Router();
 
 router.use('/auth',userRouter);
 router.use('/issues', postRouter);
+router.use('/like', likeRouter);
 
 
 module.exports = router;
