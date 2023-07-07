662b2bb5a2e7d8636f4786846d8777a59516655b
재명 <ljm9894@dgsw.hs.kr>
2023-07-07 12:22:51 +0900

    :sparkles: :: issue 태그 기능 추가
    태그 기능 추가

--- routes/board/issue.js
+++ routes/board/issue.js
@@ -2,24 +2,46 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const issue = async (req, res) => {
-  const { title, content, postImg, lat, lng } = req.body;
+  const { title, content, postImg, lat, lng, tag } = req.body;
   const userId = req.user.userId;
-  //console.log(userId);
+
+  const validTags = ['위험', '안내', '속보'];
+  if (!validTags.includes(tag)) {
+    return res.status(400).send({
+      message: '유효하지 않은 태그입니다.',
+    });
+  }
+
   try {
-    await prisma.post.create({
+    const newPost = await prisma.post.create({
       data: {
         title,
         content,
         postImg,
         longitude: parseFloat(lng),
         latitude: parseFloat(lat),
-        author : {connect : {userId : userId}},
-        deleted_at : new Date(Date.now() + 72*60*60*1000)
+        user: { connect: { userId: userId } },
+        deleted_at: new Date(Date.now() + 72 * 60 * 60 * 1000),
+        updated_at: new Date(),
+        tags: {
+          create: {
+            tag: {
+              create: {
+                tagName: tag,
+              },
+            },
+          },
+        },
+      },
+      include: {
+        tags: true,
       },
     });
+
     res.status(200).json({
       ok: true,
-      message: '게시물이 성공적으로 업데이트 되었습니다.',
+      message: '게시물이 성공적으로 업데이트되었습니다.',
+      data: newPost,
     });
   } catch (err) {
     console.log(err);
