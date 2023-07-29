5f186d62ca9feb26a00053ff0f358ffb58b10393
재명 <ljm9894@dgsw.hs.kr>
2023-07-29 16:13:31 +0900

    :sparkles: :: 이슈 조회수 추가
    issue views add

--- routes/issue/getboard.js
+++ routes/issue/getboard.js
@@ -2,18 +2,25 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const getBoard = async(req, res) =>{
-    const reqPage = req.params;
-    const page = parseInt(reqPage.page);
+    const page = parseInt(req.params.page);
     try{
-         const board = await prisma.post.findUnique({
-            where : {
+        const board = await prisma.post.findUnique({
+            where :{
                 postIdx : page
             }
-        });
+        })
+        const views = await prisma.post.update({
+            where :{
+                postIdx : page
+            },
+            data : {
+               views : board.views+1 
+            }
+        })
         return res.status(200).json({
             message : "게시물 정보 읽어옴",
             data: {
-                board
+                views
             }
         })
     }catch(err){
