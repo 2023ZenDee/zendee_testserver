84c2900a7f713e99acf1f791d6a877491d878bda
재명 <ljm9894@dgsw.hs.kr>
2023-11-01 20:52:29 +0900

    feat :: tag response 형식 form

---
+++ routes/admin/global/form/tag.js
@@ -0,0 +1,30 @@
+const tagForm = [
+  {
+    tagName: "경고",
+    value: 0,
+  },
+  {
+    tagName: "뜨거움",
+    value: 0,
+  },
+  {
+    tagName: "재미",
+    value: 0,
+  },
+  {
+    tagName: "행운",
+    value : 0,
+  },
+  {
+    tagName: "공지",
+    value: 0,
+  },
+  {
+    tagName: "활동",
+    value: 0,
+  },
+  {
+    tagName: "사랑",
+    value: 0,
+  },
+];
