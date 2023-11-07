9ada5d1fedb3a093104c3e1dc791ed80074827cf
재명 <ljm9894@dgsw.hs.kr>
2023-11-08 02:51:50 +0900

    add :: 인가 추가

--- routes/admin/statistics/index.js
+++ routes/admin/statistics/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const postByRegion = require("./regionToPost");
 const postByTag = require('./tagToPost');
 const postByTime = require('./timeToPost');
+const { authenticateUser } = require('../../../middleware/authenticate');
+const { authenticateAdmin } = require('../../../middleware/admin');
 const router = express.Router();
 
-router.get("/issues/region",postByRegion);
-router.get('/issues/tag', postByTag);
-router.get('/issues/time', postByTime);
+router.get("/issues/region",authenticateUser,authenticateAdmin,postByRegion);
+router.get("/issues/tag", authenticateUser, authenticateAdmin, postByTag);
+router.get("/issues/time", authenticateUser, authenticateAdmin, postByTime);
 
 module.exports = router;
