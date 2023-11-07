a17bec04d7ff63c34928512e3cf7c19d5a964d50
재명 <ljm9894@dgsw.hs.kr>
2023-11-07 09:48:00 +0900

    fix :: issue createdAt UTC 시간으로 변경

--- routes/issue/issue.js
+++ routes/issue/issue.js
@@ -9,7 +9,9 @@ const issue = async (req, res) => {
   const { title, content, lat, lng, tag, deleted_at } = req.body;
   const currentTime = new Date();
   currentTime.setMinutes(currentTime.getMinutes() + parseInt(deleted_at));
+  const created_at = new Date().setHours(new Date().getHours() + 20);
   currentTime.setHours(currentTime.getHours() + 9);
+
   const expired_at = currentTime;
   const validTags = ["경고", "뜨거움", "재미", "행운", "공지", "활동", "사랑"];
   if (!validTags.includes(tag)) {
@@ -24,13 +26,8 @@ const issue = async (req, res) => {
   }
 
   try {
-    const filePath = !req.file ? null :req.file.location;
-    // if(!filePath){
-    //   return res.status(401).send(
-    //     authUtil.successTrue(statusCode.UNAUTHORIZED, responseMessage.INVALID_FILE)
-    //   )
-    // }
-    
+    const filePath = !req.file ? null : req.file.location;
+
     const address = await getAddress(lat, lng);
     if (!address) {
       return res
@@ -51,6 +48,7 @@ const issue = async (req, res) => {
         latitude: parseFloat(lat),
         address: address,
         user: { connect: { userIdx: req.user.userIdx } },
+        created_at: new Date(created_at),
         deleted_at: expired_at,
         updated_at: new Date(),
         tags: {
